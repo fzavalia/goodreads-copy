85d2c10d9201129db94a87f4640ec200ff372dcb
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-03-19 18:54:14 -0300

    Add second row

---
+++ app/components/header/header.tsx
@@ -0,0 +1,36 @@
+import styles from "./header.module.sass";
+import classnames from "classnames";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.firstRow}>
+        <span>
+          <img
+            className="h-5 ml-4 w-6"
+            src="https://www.goodreads.com/assets/layout/header/icn_nav_search_black.svg"
+          />
+        </span>
+        <span>
+          <img
+            className="h-7"
+            src="https://www.goodreads.com/assets/layout/header/goodreads_logo.svg"
+          />
+        </span>
+        <span className="text-right">
+          <img
+            className="h-8 rounded-full border-2 mr-4"
+            src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/users/1615778541i/113470954._UY60_CR10,0,60,60_.jpg"
+          />
+        </span>
+      </div>
+      <div className={styles.secondRow}>
+        <div>My Books</div>
+        <div>Browse ▾</div>
+        <div>Community ▾</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
